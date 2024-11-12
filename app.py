964b3d3dcc5456a07841252fb2772248a5f2de24
Franco Benito <francoh298@gmail.com>
2024-11-11 23:27:44 -0300

    SQLite to Postgres

--- app.py
+++ app.py
@@ -1,29 +1,52 @@
+from mutant import *
 from fastapi import FastAPI, status, Response
 from contextlib import asynccontextmanager
 from typing import List
 from pydantic import BaseModel
-import sqlite3
 import json
-from mutant import *
+import databases
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.future import select
+
 
 DB_PATH = "database.db"
+DATABASE_URL = "postgresql://default:WahU5oSNL3pK@ep-solitary-sound-a4cexntd-pooler.us-east-1.aws.neon.tech:5432/verceldb"
+database = databases.Database(DATABASE_URL)
+Base = declarative_base()
+
+
+class Dna(Base):
+    __tablename__ = 'dna'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    dna = Column(String, nullable=False)
+
+
+class Stats(Base):
+    __tablename__ = 'stats'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    count_mutant_dna = Column(Integer, default=0)
+    count_human_dna = Column(Integer, default=0)
+
+
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(engine)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the database
-    database_connection = sqlite3.connect(DB_PATH)
-    database_connection.execute(
-        """CREATE TABLE IF NOT EXISTS dna (id INTEGER PRIMARY KEY AUTOINCREMENT, dna TEXT, mutant INTEGER)""")
-    database_connection.execute(
-        """CREATE TABLE IF NOT EXISTS stats (id INTEGER PRIMARY KEY AUTOINCREMENT, count_mutant_dna INTEGER DEFAULT 0, 
-        count_human_dna INTEGER DEFAULT 0)""")
-    database_connection.execute(
-        """INSERT INTO stats(count_mutant_dna, count_human_dna) VALUES (0, 0)""")
-    app.state.db = database_connection
+    await database.connect()
+    query = "SELECT COUNT(*) FROM stats"
+    result = await database.fetch_one(query)
+    if result[0] == 0:
+        # Insert default stats if table is empty
+        query = "INSERT INTO stats(count_mutant_dna, count_human_dna) VALUES (0, 0)"
+        await database.execute(query)
+
     yield
     # Close the connection
-    database_connection.close()
+    await database.disconnect()
 
 app = FastAPI(lifespan=lifespan)
 
@@ -34,32 +57,32 @@ class Mutant(BaseModel):
 
 @app.get("/stats")
 async def get_stats():
-    cursor = app.state.db.execute(
-        "SELECT count_mutant_dna, count_human_dna from stats")
-    stats = cursor.fetchone()
-    if stats[0] != 0 and stats[1] != 0:
-        ratio = stats[0] / stats[1]
+    query = "SELECT count_mutant_dna, count_human_dna FROM stats WHERE id = 1"
+    stats = await database.fetch_one(query)
+
+    if stats['count_human_dna'] != 0:
+        ratio = stats['count_mutant_dna'] / stats['count_human_dna']
     else:
-        ratio = 0
+        ratio = stats['count_mutant_dna']
+
     return {
-        "count_mutant_dna": stats[0],
-        "count_human_dna": stats[1],
-        "ratio": ratio
+        "count_mutant_dna": stats['count_mutant_dna'],
+        "count_human_dna": stats['count_human_dna'],
+        "ratio": "{:.1f}".format(ratio) # return only one decimal
     }
 
 
 @app.post("/mutant", status_code=status.HTTP_200_OK)
 async def get_mutant(mutant: Mutant, response: Response):
-    app.state.db.execute(
-        """INSERT INTO dna(dna) VALUES (?)""", (json.dumps(mutant.dna), ))
-    app.state.db.commit()
+    dna = json.dumps(mutant.dna)
+    query = "INSERT INTO dna(dna) VALUES (:dna)"
+    await database.execute(query, values={"dna": dna})
+    
     if is_mutant(mutant.dna):
-        app.state.db.execute(
-            """UPDATE stats SET count_mutant_dna = count_mutant_dna + 1 WHERE id = 1""")
-        app.state.db.commit()
+        query = "UPDATE stats SET count_mutant_dna = count_mutant_dna + 1 WHERE id = 1"
+        await database.execute(query)
         return True
     else:
-        app.state.db.execute(
-            """UPDATE stats SET count_human_dna = count_human_dna + 1 WHERE id = 1""")
-        app.state.db.commit()
+        query = "UPDATE stats SET count_human_dna = count_human_dna + 1 WHERE id = 1"
+        await database.execute(query)
         response.status_code = status.HTTP_403_FORBIDDEN
